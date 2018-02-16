9f48b26fe359a1b8ef31e7dcd260518b0301c307
jumproper <dylanmplummer@gmail.com>
2018-02-15 19:39:01 -0500

    return value now stored in state

--- interpreter.rkt
+++ interpreter.rkt
@@ -10,7 +10,7 @@
 (define run
   (lambda (parsetree state)
     (if (null? parsetree)
-      state
+      (searchVar 'return state)
       (run (cdr parsetree) (stateGlobal (car parsetree) state)))))
 
 ;stateGlobal takes a statement and a state and returns the new state after
@@ -19,7 +19,7 @@
   (lambda (statement state)
     (cond
       ((eq? (car statement) 'while) (stateWhile (whileConditon statement) (whilebody statement) state))
-      ((eq? (car statement) 'return) (value (cadr statement)))
+      ((eq? (car statement) 'return) (stateReturn (cadr statement) state))
       (else (error "Incorrect syntax")))))
 
 ;stateWhile takes a while loop condition, a loop body statement, and a stateEmpty
@@ -34,6 +34,10 @@
 (define whileBody
   caddr)
 
+(define stateReturn
+  (lambda (expression state)
+    (addToState 'return (value expression) (removeVar 'return state))))
+
 ;value takes an expression and returns it's value
 (define value
   (lambda (expression)
@@ -45,28 +49,35 @@
 (define stateEmpty
   '(() ()))
 
+(define names
+  car)
+(define vals
+  cdr)
+
 ;addToState takes a variable and data and adds it to a state
 (define addToState
   (lambda (var data state)
     (if (null? state)
-      (cons (list var) (list data))
-      (cons (cons var (car state)) (cons data (cadr state))))))
+      (list (list var) (list data))
+      (list (cons var (names state)) (cons data (vals state))))))
 
 ;searchVar takes a var and a state and returns associated data
 (define searchVar
   (lambda (var state)
+    (display state) (newline)
     (cond
-      ((null? state) null)
-      ((null? (cdr state)) null)
+      ((null? state) stateEmpty)
+      ;((null? (cdr state)) null)
       ((eq? var (caar state)) (cadr state))
-      (else (searchVar var (cdr state))))))
+      (else (searchVar var (list (cdr (name state)) (cdr (vals state))))))))
 
 ;removeVar takes a var and removes it and returns the new state
 ;We should abstract some of the repetitive cars and cdrs
 (define removeVar
   (lambda (var state)
     (cond
-      ((null? var) state ) ;null var, return given state
-      ((null? state) '('() '()) ) ;null state, return stateEmpty
+      ((null? var) state) ;null var, return given state
+      ((null? state) stateEmpty) ;null state, return stateEmpty
+      ((or (null? (names state)) (null? (vals state))) stateEmpty)
       ((eq? var (car (car state))) (cons (cadr (car state)) (cadr (cadr state)))) ;var match, return everything else
       (else (cons (cons (cdr (car state)) (cdr (cadr state))) (removeVar var (cons (cdr (car state)) (cdr (cadr state)))))))))
